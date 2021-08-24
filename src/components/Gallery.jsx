b31f6f9826d81497949cb32b9589a08b75cb61d3
AgbetiFavour <agbetifavour1@gmail.com>
2021-08-24 09:23:03 -0700

    instra-redux

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import getPhotoUrl from 'get-photo-url'
-import { posts } from '../data'
+import { useDispatch, useSelector } from 'react-redux'
+import { galleryActions } from '../data/slice'
 
 const Gallery = () => {
+
+  const galleryState = useSelector((state) => state.gallery)
+  const dispatch = useDispatch()
+
   const addPhoto = async () => {
     const imageUrl = await getPhotoUrl('#addPhotoInput')
     const post = {
@@ -10,9 +15,12 @@ const Gallery = () => {
       imageUrl,
       text: ''
     }
+    dispatch(galleryActions.addPost(post))
   }
 
-  const galleryItems = posts.map(post => (
+
+
+  const galleryItems = galleryState.posts.map(post => (
     <div className="item" key={post.id} tabIndex={8}>
       <img src={post.imageUrl} alt="" />
     </div>
